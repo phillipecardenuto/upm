24f42d214cd76ac144f5a18e659b7619ebd9a365
João Phillipe Cardenuto <phillipe.cardenuto@gmail.com>
2023-10-16 16:01:19 -0300

    organize provenance dir

--- provenance/ours/forensic_lib/utils/exceptions.py
+++
@@ -1,7 +0,0 @@
-# class Error(Exception):
-#     """Base class for other exceptions"""
-#     pass
-
-class ElementIdNotFound(Exception):
-    """Raised when ElementId not found on Database"""
-    pass
